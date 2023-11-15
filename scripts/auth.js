4f4873e74a790527d40b08f9348ca56df0150a21
Laban Mutwiwa <labanmutwiwa833@gmail.com>
2023-11-15 15:22:11 +0300

    adding auth

--- scripts/auth.js
+++ scripts/auth.js
@@ -9,3 +9,18 @@ const firebaseConfig = {
     appId: "1:532294208063:web:36a60b7ce32bc62f8e868a",
     measurementId: "G-NWLS46EMF4"
   };
+
+  const signupButton = document.getElementById("sign-up");
+  const returnBtn = document.getElementById("return-btn");
+  const createacctSection = document.getElementById("create-acct");
+  const main = document.getElementById("main");
+
+  signupButton.addEventListener("click", () => {
+    main.style.display = "none";
+    createacctSection.style.display = "block";
+  });
+ 
+  returnBtn.addEventListener("click", () => {
+    main.style.display = "block";
+    createacctSection.style.display = "none";
+  })
