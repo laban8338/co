7fc5b638eebce9fc67422db32e3fd7e95f41e11f
Laban Mutwiwa <labanmutwiwa833@gmail.com>
2023-12-05 22:34:20 +0300

    task creation

---
+++ scripts/createTask.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC5WSlpZ6YDYqJnr16rMmlziG6h9stN82M",
+    authDomain: "compassion-todo-app-ef2a3.firebaseapp.com",
+    databaseURL: "https://compassion-todo-app-ef2a3-default-rtdb.firebaseio.com",
+    projectId: "compassion-todo-app-ef2a3",
+    storageBucket: "compassion-todo-app-ef2a3.appspot.com",
+    messagingSenderId: "532294208063",
+    appId: "1:532294208063:web:36a60b7ce32bc62f8e868a",
+    measurementId: "G-NWLS46EMF4"
+  };
+
+  //   Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
